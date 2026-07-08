16ebc35e215156c8765f0333c92e560960aedcbc
Sowjanya1324 <souji.friendlyworld@gmail.com>
2026-07-08 21:59:05 +0530

    Added sample Rest end point

--- test-app/src/main/java/com/test/testapp/TestAppApplication.java
+++ test-app/src/main/java/com/test/testapp/TestAppApplication.java
@@ -2,13 +2,19 @@ package com.test.testapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TestAppApplication {
+	
+	@GetMapping
+	public String getMsg() {
+		return "Hello World!!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
-		System.out.println("Hello World!!!");
+		
 	}
 
 }
